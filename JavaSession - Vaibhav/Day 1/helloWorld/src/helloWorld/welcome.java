87dc3b9215da6ae98bbbbd0af3a169e8e16afaed
Nikhil <nikhilshah.net@hotmail.com>
2023-05-13 15:22:35 +0530

    Day 1

---
+++ JavaSession - Vaibhav/Day 1/helloWorld/src/helloWorld/welcome.java
@@ -0,0 +1,85 @@
+package helloWorld;
+import java.util.Scanner;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		
+		Scanner readInput = new Scanner(System.in);
+//		
+//		System.out.print("Hello and Welcome to Java World");
+//				
+//		String guestName = readInput.nextLine();
+//		
+//		System.out.print("\n Welcome : " + guestName);
+//		
+//		
+//		System.out.print("\n Enter First Number : ");
+//		float firstNum = readInput.nextFloat();
+//		
+//		System.out.print("\n Enter Second Number : ");
+//		float secondNum = readInput.nextFloat();		
+//		
+//		float addition = firstNum + secondNum;
+//		float subtraction = firstNum - secondNum;
+//		float multiplication = firstNum * secondNum;
+//		float division = firstNum / secondNum;
+//		
+//		System.out.print("\n Addition is : " + addition);
+//		System.out.print("\n Subtraction is : " + subtraction);
+//		System.out.print("\n Multiplication is : " + multiplication);
+//		System.out.print("\n Division is : " + division);
+		
+		
+//		System.out.print("Enter A Number : " );
+//		int num = readInput.nextInt();
+//		
+//		
+//		if(num < 0)
+//		{
+//			System.out.print("Please Enter a positive number");
+//		}
+//		else if(num > 0 && num < 10)
+//		{
+//			System.out.print("Number accepted by does not give a good vibe");			
+//		}
+//		else if(num > 10 && num < 25)
+//		{
+//			System.out.print("Good number - good vibe");
+//		}
+//		else if(num >= 25  && num <= 50)
+//		{
+//			System.out.print("Very good number - very good vibe");
+//		}
+//		else if(num > 50)
+//		{
+//			System.out.print("Excellent  number - excellent vibe");
+//		}
+//		else
+//		{
+//			System.out.print("Something went wrong");
+//		}
+		
+	
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
