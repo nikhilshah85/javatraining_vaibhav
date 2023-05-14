f1c2b5da380e409a36dcb2be3061a69d9c7795c7
nikhilshah.net@hotmail.com <nikhilshah.net@hotmail.com>
2023-05-14 15:18:52 +0530

    Day 2 - Final

--- JavaSession - Vaibhav/Day 2/loopdemo/loopdemo/loopsdemo.java
+++ JavaSession - Vaibhav/Day 2/loopdemo/loopdemo/loopsdemo.java
@@ -10,47 +10,105 @@ public class loopsdemo {
 		//b. for loop - use this when you know number of times of iteration 
 		
 		Scanner read = new Scanner(System.in);
-		System.out.print("Keep Entering  number, enter 0 when done ");
-			
-		int evenNumbers = 0;
-		int oddNumbers = 0;
-		int total = 0;
-		int negatives = 0;
-		int count = 0;
+//		System.out.print("Keep Entering  number, enter 0 when done ");
+//			
+//		int evenNumbers = 0;
+//		int oddNumbers = 0;
+//		int total = 0;
+//		int negatives = 0;
+//		int count = 0;
+//		
+//		int favNumber = read.nextInt();
+//		
+//		while(favNumber != 0)
+//		{
+//			
+//			count = count + 1;
+//			total = total + favNumber;
+//			
+//			
+//			if(favNumber % 2 == 0)
+//			{
+//				evenNumbers = evenNumbers + 1;
+//			}
+//			else
+//			{
+//				oddNumbers = oddNumbers + 1;
+//			}
+//			if(favNumber < 0)
+//			{
+//				negatives = negatives + 1;
+//			}
+//			System.out.print("Enter Fav number");
+//			
+//			favNumber = read.nextInt();	
+//					
+//		}
+//		System.out.print("Thank you for  numbers, below is the summary");
+//		
+//		System.out.println("Total Numbers you entered : " + count );
+//		System.out.println("Sum of Numbers you entered : " + total );
+//		System.out.println("Total Even Numbers you entered : " + evenNumbers );
+//		System.out.println("Total Odd Numbers you entered : " + oddNumbers );
+//		System.out.println("Total Negative Numbers you entered : " + negatives );
 		
-		int favNumber = read.nextInt();
+		Boolean isSuccess = false;
+		int number = 0;
 		
-		while(favNumber != 0)
+		while (!isSuccess) 
+		{
+		
+		
+		try
 		{
 			
-			count = count + 1;
-			total = total + favNumber;
+			System.out.print("Enter A Number ");
+			number = read.nextInt();
 			
-			
-			if(favNumber % 2 == 0)
-			{
-				evenNumbers = evenNumbers + 1;
-			}
-			else
-			{
-				oddNumbers = oddNumbers + 1;
-			}
-			if(favNumber < 0)
-			{
-				negatives = negatives + 1;
-			}
-			System.out.print("Enter Fav number");
-			
-			favNumber = read.nextInt();	
-					
-		}
-		System.out.print("Thank you for  numbers, below is the summary");
 		
-		System.out.println("Total Numbers you entered : " + count );
-		System.out.println("Sum of Numbers you entered : " + total );
-		System.out.println("Total Even Numbers you entered : " + evenNumbers );
-		System.out.println("Total Odd Numbers you entered : " + oddNumbers );
-		System.out.println("Total Negative Numbers you entered : " + negatives );
+		 
+		
+		for(int i=0;i < number; i++)
+		{
+		System.out.println(" Hello : " + (i +1));
+		}
+		isSuccess = true;
+		
+		}
+		catch(Exception es)
+		{
+			System.out.println(es);
+			System.out.println("Please enter only numeric value");
+		}
+		finally {
+			System.out.println("Thank you");;
+		}
+		
+		
+		}
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
